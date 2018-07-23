87c8f60fe3e7981ff0ae0f41c8ce8b1838eea2fd
gwhalley <41588850+gwhalley@users.noreply.github.com>
2018-07-23 13:29:59 -0400

    Report all active users not in cloud
    Report all active users not in cloud

--- findonprem.ps1
+++ findonprem.ps1
@@ -1,3 +1,3 @@
-#Find all users active users that are not in the cloud
+#Find all active AD users that are not in the cloud
 import-module ActiveDirectory
-get-aduser -SearchBase "YOURDN" -filter {enabled -eq $true} -Properties *| where {$_.targetaddress -notlike "*onmicrosoft.com"} | select mail
+get-aduser -SearchBase "YOURDN" -filter {enabled -eq $true} -Properties *| where {$_.targetaddress -notlike "*onmicrosoft.com"} | select mail | export-csv onprem.csv
