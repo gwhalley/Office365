e289df71404080094109240f7ff6e5de63c6f4d7
gwhalley <41588850+gwhalley@users.noreply.github.com>
2018-07-23 13:03:07 -0400

    Email to DN Reverse Lookup
    Email to DN Reverse Lookup

---
+++ emailtoDN.ps1
@@ -0,0 +1,8 @@
+﻿$csv = Import-CSV email.csv
+foreach($user in $csv){
+    $userMail = $user.Email
+    $userDN = Get-ADUser -Filter {mail -eq $userMail} -Properties Distinguishedname
+	foreach ($userID in $userDN) {
+		add-content -Path "tor-hemlo.csv" -Value $userID.Distinguishedname
+	}
+}
